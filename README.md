c85e44e45bbd2c0fbac4001eebd39750325e60ec
Álan Hideyuki Shishido <alan.shishido@fpf.br>
2023-07-08 12:42:59 -0400

    Connecting API client

--- README.md
+++ README.md
@@ -6,36 +6,51 @@ Practicing about react front end development
 * [Install Node](https://nodejs.org/)
 * [Install Yarn](https://classic.yarnpkg.com/lang/en/docs/install/#windows-stable)
 * [Install create-react-app](https://create-react-app.dev/docs/getting-started/)
+
+## First Steps:
+
 * Create Project: ``$ create-react-app [react-project-name] --template=typescript``
-
-### First Steps:
-
 1. Deleting archives:
     - README.md
     - src/*.css
 2. Modify Dependeces
 3. On cmd: ``$ yarn start``
 
-### Development:
+## Development:
+
+### Creating Routes
+
 1. Install Router in project:
 ```
 $ yarn react-router-dom
 $ yarn @types/react-router-dom -D
 ```
-2. create path and archive: src/routes/index.tsx
-3. create path: src/pages/
-4. Install Styled Components to project:
+2. create path and archive: `src/routes/index.tsx`
+3. create path: `src/pages/`
+
+### Using Styled Components
+1. Install Styled Components to project:
 ```
 $ yarn add styled-components
 $ yarn add @types/styled-components -D
 ```
-5. create path and archive: styles/global.ts
-6. Install Polish:
+2. create path and archive: `src/styles/global.ts`
+
+### Styling Dashboard
+1. Install Polish:
 ```
 $ yarn add polished
 ```
-7. Install React Icons: 
+2. Install React Icons: 
 ```
 $ yarn add react-icons
-$ yarn add @types/polished -D
+```
+
+### Connecting to API
+1. create path and archive: `src/services/client.ts` ou `src/services/api.ts`
+2. install axios:
+
+```
+$ yarn add axios
+$ yarn add @types/axios -D
 ```
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,48 +1,76 @@
-import React from "react";
-import { FiChevronRight } from 'react-icons/fi'
+import React, { useState, FormEvent } from "react";
+import { FiChevronRight } from 'react-icons/fi';
 
-import logoImg from "../../assets/logo.svg"
+import client from '../../services/client';
+import logoImg from "../../assets/logo.svg";
 
-import { Title, Form, Repositories } from "./styles"
+import { Title, Form, Repositories } from "./styles";
+import Repository from "../Repository";
+
+interface Repository {
+    full_name: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+    description: string;
+    html_url: string;
+}
 
 const Dashboard: React.FC = () => {
+    const [newRepo, setNewRepo] = useState('')
+    const [repositories, setRepositories] = useState<Repository[]>([])
+
+    async function handleAddReposity(event: FormEvent){
+        event.preventDefault();
+        
+        const response = await client.get<Repository>(`repos/${newRepo}`);
+
+        // console.log(response.data)
+
+        const repository = response.data;
+
+        setRepositories([... repositories, repository]);
+        setNewRepo('');
+    }
+
     return (
         <>
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no GitHub</Title>
 
-            <Form action="">
-                <input type="text" placeholder="Digite o nome do repositório" />
+            <Form onSubmit={handleAddReposity}>
+                <input type="text"
+                    placeholder="Digite o nome do repositório" 
+                    value={newRepo}
+                    onChange={e => setNewRepo(e.target.value)}
+                    />
                 <button type="submit">Pesquisar</button>
             </Form>
 
             <Repositories>
-                <a href="teste">
+                {/* <a href="teste">
                     <img src="https://avatars.githubusercontent.com/u/51389025?v=4" alt="Alan Shishido" />
                     <div>
                         <strong>rockeseat/uniform</strong>
                         <p>Easy peasy highly scalable ReacjJS & React Native forms!</p>
                     </div>
-                    <FiChevronRight size={20}/>
-                </a>
-                
-                <a href="teste">
-                    <img src="https://avatars.githubusercontent.com/u/51389025?v=4" alt="Alan Shishido" />
-                    <div>
-                        <strong>rockeseat/uniform</strong>
-                        <p>Easy peasy highly scalable ReacjJS & React Native forms!</p>
-                    </div>
-                    <FiChevronRight size={20}/>
-                </a>
+                    <FiChevronRight size={20} />
+                </a> */}
 
-                <a href="teste">
-                    <img src="https://avatars.githubusercontent.com/u/51389025?v=4" alt="Alan Shishido" />
-                    <div>
-                        <strong>rockeseat/uniform</strong>
-                        <p>Easy peasy highly scalable ReacjJS & React Native forms!</p>
-                    </div>
-                    <FiChevronRight size={20}/>
-                </a>
+                {repositories.map((repo) => {
+                    return (
+                        <a key={repo.full_name} href={repo.html_url}>
+                            <img src={repo.owner.avatar_url} alt={repo.owner.login} />
+                            <div>
+                                <strong>{repo.full_name}</strong>
+                                <p>{repo.description}</p>
+                            </div>
+                            <FiChevronRight size={20} />
+                        </a>
+                    )
+                })}
+
             </Repositories>
         </>
     )
